d8566b2f2106afe8f7bb88c40c4100d4ec600964
Jay Nelson <jay@duomark.com>
2013-01-10 14:06:05 -0800

    Add unit test to ensure a new value added to cache doesn't block existing key fetches

--- test/pcache_tests.erl
+++ test/pcache_tests.erl
@@ -1,7 +1,7 @@
 -module(pcache_tests).
 -include_lib("eunit/include/eunit.hrl").
 
--export([tester/1, memoize_tester/1]).
+-export([tester/1, memoize_tester/1, slow_tester/1]).
 
 -define(E(A, B), ?assertEqual(A, B)).
 -define(_E(A, B), ?_assertEqual(A, B)).
@@ -10,7 +10,7 @@ pcache_setup() ->
   % start cache server tc (test cache)
   % 6 MB cache
   % 5 minute TTL per entry (300 seconds)
-  {ok, Pid} = pcache_server:start_link(tc, pcache_tests, tester, 6, 300000),
+  {ok, Pid} = pcache_server:start_link(tc, ?MODULE, tester, 6, 300000),
   Pid.
 
 pcache_cleanup(Cache) ->
@@ -44,6 +44,10 @@ pcache_test_() ->
     end
   }.
   
+
+%%% =======================================================================
+%%% Test the speed of gets when the gen_server message queue is full
+%%% =======================================================================
 pcache_queue_test_() ->
     {setup, fun pcache_setup/0, fun pcache_cleanup/1,
      {with, [fun check_msg_queue_speed/1]}
@@ -87,4 +91,57 @@ get_results(Count) ->
     receive {datum, _Result} -> get_results(Count-1)
     after 3000 -> timeout
     end.                         
+
+
+%%% =======================================================================
+%%% Test that slow new value M:F(A) doesn't stall get requests
+%%% =======================================================================
+pcache_slow_setup() ->
+  % start cache server tc (test cache)
+  % 6 MB cache
+  % 5 minute TTL per entry (300 seconds)
+  {ok, Pid} = pcache_server:start_link(tc, ?MODULE, slow_tester, 6, 300000),
+  Pid.
+
+slow_tester(Key) when is_binary(Key) orelse is_list(Key) ->
+    timer:sleep(1000),
+    erlang:md5(Key).
                                      
+pcache_spawn_test_() ->
+    {setup, fun pcache_slow_setup/0, fun pcache_cleanup/1,
+     {with, [fun check_spawn_speed/1]}
+     }.
+
+fetch_timing(Cache, Existing_Key, New_Key) ->
+    Caller = self(),
+    Notify_Fn = fun() ->
+                        {Micros, Result} = timer:tc(fun() -> pcache:get(Cache, Existing_Key) end),
+                        Caller ! {datum, Existing_Key, Micros, Result}
+                end,
+
+    %% Attempt to plug up the server generating a new key...
+    spawn(fun() -> pcache:get(Cache, New_Key) end),
+    %% While waiting for existing key fetches.
+    [spawn(Notify_Fn) || _N <- lists:seq(1,5)],
+
+    get_key_results(5, []).
+
+get_key_results(0,     Results) -> Results;
+get_key_results(Count, Results) ->
+    receive Datum -> get_key_results(Count-1, [Datum | Results])
+    after 3000 -> timeout
+    end.                         
+    
+check_spawn_speed(Cache) ->
+    Existing_Result = erlang:md5("existing_key"),
+    {Micros_Existing, Get_Existing_New} = timer:tc(fun() -> pcache:get(Cache, "existing_key") end),
+    ?assertMatch(Existing_Result, Get_Existing_New),
+    ?assert(1000000 < Micros_Existing),
+
+    Results = fetch_timing(Cache, "existing_key", "created_key"),
+    ?assertMatch(5, length(Results)),
+    Slow_Fetches = [[{latency, Micros}, {key, Key}, {result, Result}]
+                    || {datum, Key, Micros, Result} <- Results, Micros > 100],
+    ?assertMatch([], Slow_Fetches),
+    ?assertMatch([], [R || {datum, _Key, _Micros, R} <- Results, R =/= Existing_Result]).
+                            
